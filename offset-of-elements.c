c724b17254e4626dcc52999cd81754785d0f0406
hladkou <yar@wavedecompiler.>
2023-11-27 11:58:03 +0300

    Update repo

--- offset-of-elements.c
+++ offset-of-elements.c
@@ -4,26 +4,22 @@
 
 struct TestStruct
 {
-    char c; // size = 1 byte
-    int i;  // size = 4 bytes
+    char c;
+    int x;
 };
 
 int main()
 {
+
     // Print size of struct
     printf("Size of TestStruct: %zu bytes\n", sizeof(struct TestStruct));
+
     // Print alignment requirement
     printf("Alignment of int: %zu\n", alignof(int));
+
     // Print offset of elements
     printf("Offset of c: %zu\n", offsetof(struct TestStruct, c));
-    printf("Offset of i: %zu\n", offsetof(struct TestStruct, i));
-
-    struct TestStruct a;
-
-    // A padding of 3 bytes added after char to align int
-    printf("a.c (char) = %lu\n", sizeof(a.c));
-    printf("a.i (int) = %lu\n", sizeof(a.i));
-    printf("whole struct takes %lu bytes\n", sizeof(a)); // = 1 + 3 pad bytes + 4 = 8 bytes
+    printf("Offset of x: %zu\n", offsetof(struct TestStruct, x));
 
     return 0;
 }
